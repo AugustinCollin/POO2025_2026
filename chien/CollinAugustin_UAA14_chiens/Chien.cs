fd4bac919bd2d6626305900dec37e2024df9177b
AugustinCollin <240400@site.asty-moulin.be>
2025-09-29 10:03:15 +0200

    travail sur les chiens
    création de la première classe

---
+++ chien/CollinAugustin_UAA14_chiens/Chien.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollinAugustin_UAA14_chiens
+{
+    internal class Chien
+    {
+        //attributs
+        private int _age; // age du chien 
+        private string _nom; // nom du chien 
+        private string _race; // race du chien 
+        private int _poids; // poids du chien 
+
+        //constructeur
+        public Chien(int age, int poids, string nom, string race)
+        {
+            _nom = nom;
+            _age = age;
+            _race = race;
+            _poids = poids; 
+        }
+        //methodes
+        public string AfficheCaractéristiques()
+        {
+            return $"le chien a {_age} an(s) il sappelle {_nom} c'est un {_race} il pèse {_poids} killos" ;
+        }
+        public string Aboyer()
+        {
+            return "waf waf";
+        }
+        public void vieillir() 
+        {
+            _age += 1;
+        }
+    }
+}
---
+++ chien/CollinAugustin_UAA14_chiens/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace CollinAugustin_UAA14_chiens
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Chien monChien = new Chien(2, 10, "Medor", "ChiWaWa");
+            Console.WriteLine(monChien.AfficheCaractéristiques());
+            Console.WriteLine(monChien.Aboyer());
+            monChien.vieillir();
+            Console.WriteLine(monChien.AfficheCaractéristiques());
+
+            Chien[] mesChien = new Chien[3];
+            mesChien[0] = new Chien(2, 5, "rex", "tekel");
+            mesChien[1] = new Chien(3, 12, "spinner", "bouvier des flandres");
+            mesChien[2] = new Chien(1, 10, "luna", "border collie");
+
+            for (int iChien = 0; iChien < mesChien.Length; iChien++)
+            {
+                Console.WriteLine(mesChien[iChien].AfficheCaractéristiques());
+                mesChien[iChien].vieillir();
+            }
+
+        }
+    }
+}
